1c3994f9cba343b667a8cf54bbb5733083f54701
jairoalves <jairoalves@users.noreply.github.com>
2021-05-16 03:12:04 -0300

    Versão com Waits e saneada, com Escriturador correto.

--- obtem_dados_BDRs_B3.py
+++ obtem_dados_BDRs_B3.py
@@ -2,22 +2,32 @@ import pandas as pd
 
 import time
 import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from msedge.selenium_tools import Edge, EdgeOptions
 
+from util.utilitarios_gerais import gera_saida_em_excel
+
 class BDR(dict):
 
-    def __init__(self, els):
+    def __init__(self, nome, els, escriturador='-'):
 
         dados_bdr = {
-            'nome':	els[0].text.strip(),
+            'nome':	nome,
+            'Nome de Pregão': els[0].text.strip(),
             'Códigos de Negociação': els[1].text.strip().replace('Mais Códigos\n', ''),
             'CNPJ':	els[2].text.strip(),
             'Classificação Setorial': els[4].text.strip(),
             'Site': els[5].text.strip(),
-            'Quantidade': els[6].text.strip(),
-            'Escriturador': 'BANCO B3 S.A.',
+            'Escriturador': escriturador,
         }
+
+        try:
+            dados_bdr['Quantidade'] = els[6].text.strip()
+        except:
+            dados_bdr['Quantidade'] = '-'
 
         super(BDR, self).__init__(dados_bdr)
 
@@ -42,6 +52,9 @@ class AcessoBDRsB3(Edge):
         self.xpath_botao_cookies = "/html/body/div[3]/div[3]/div/div/div[2]/div/button[2]"
         self.id_iframe = "ctl00_contentPlaceHolderConteudo_iframeCarregadorPaginaExterna"
         self.xpath_dados_bdr = "//td[2]"
+        self.css_aba_contatos = 'body > div:nth-child(4) > div.large-8.columns > ul > li:nth-child(2)'
+        self.xpath_escriturador = '//*[@id="divContatos"]//td[2]'
+        self.css_escriturador = '#divContatos > div > table > tbody > tr > td:nth-child(2)'
         self.seletores_tab_bdr = '#tblBdrs > tbody > tr > td.razaoSocial > a'
 
     def __init__(self):
@@ -52,7 +65,9 @@ class AcessoBDRsB3(Edge):
         self.options = EdgeOptions()
         self.options.use_chromium = True
 
-        super().__init__(executable_path=self.edge_webdriver)
+        super().__init__(
+            executable_path=self.edge_webdriver,
+            options=self.options)
 
     def abre_pag_inicial_BDRs(self):
         self.get(self.url_pag_inicial_BDRs)
@@ -78,60 +93,90 @@ class AcessoBDRsB3(Edge):
             info_pag_bdr = {'nome': el.text.strip(), 'link': el.get_attribute('href')}
             self.info_pags_bdrs.append(info_pag_bdr)
     
-    def salva_excel_info_pags_BDRs(self):
-        df_links_bdrs = pd.DataFrame(self.info_pags_bdrs).set_index('nome')
-        df_links_bdrs.to_excel(self.end_arq_info_pags_bdrs)
-        return df_links_bdrs
-    
-    def salva_excel_detalhes_BDRs(self):
-        df_detalhes_bdrs = pd.DataFrame(self.lista_detalhes_bdrs).set_index('nome')
-        df_detalhes_bdrs.to_excel(self.end_arq_detalhes_bdrs)
-        return df_detalhes_bdrs
-    
     def gera_detalhes_lista_BDRs(self, max=0):
         """Max permite dar um limite máximo de itens a coletar"""
-        if max == 0:
-            max = len(self.info_pags_bdrs)
 
         df_links = pd.read_excel(self.end_arq_info_pags_bdrs)
 
+        if max == 0:
+            max = len(df_links)
+
         # para cada BDR da lista geral
-        for idx, info_bdr in self.info_pags_bdrs[:max]:
+        for idx, info_bdr in df_links.iterrows():
             # obtem seus detalhes
             nome_bdr, link_bdr = info_bdr.tolist()
-            print(f'Fazendo a coleta dos dados do BDR: {nome_bdr}..')
-            bdr_atual = self.obtem_detalhes_um_bdr(link_bdr)
+            print(f'\nFazendo a coleta dos dados do BDR: {nome_bdr}..')
+            bdr_atual = self.obtem_detalhes_um_bdr(nome_bdr, link_bdr)
             
             print('\n\n')
             print(bdr_atual)
         
             # salva detalhes do BDR atual na lista completa
             self.lista_detalhes_bdrs.append(bdr_atual)
+
+            if idx >= max:
+                break
     
-    def obtem_detalhes_um_bdr(self, url_bdr):
+    def obtem_detalhes_um_bdr(self, nome_bdr, url_bdr):
         self.get(url_bdr)
-        time.sleep(3)
-
+        
+        # Seleciona o frame
+        WebDriverWait(self, 20).until(
+            EC.visibility_of_element_located(
+                (By.ID, self.id_iframe)))
         el_iframe = self.find_element_by_id(self.id_iframe)
-
         self.switch_to.frame(el_iframe)
 
-        els = self.find_elements_by_xpath(self.xpath_dados_bdr)
-        bdr_atual = BDR(els)
+        # Pega dados da Companhia
+        WebDriverWait(self, 20).until(
+            EC.visibility_of_element_located(
+                (By.XPATH, self.xpath_dados_bdr)))
+        els_dados_cia = self.find_elements_by_xpath(self.xpath_dados_bdr)
+
+        # carrega dados da aba "Dados da Companhia"
+        bdr_atual = BDR(nome_bdr, els_dados_cia)
+
+        # carrega dados da aba "Contatos"
+        el_aba_contatos = self.find_element_by_css_selector(self.css_aba_contatos)
+        el_aba_contatos.click()
+        escriturador = WebDriverWait(self, 20).until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, self.css_escriturador))).text
+        
+        bdr_atual['Escriturador'] = escriturador
 
         return bdr_atual
+    
+    def salva_excel_info_pags_BDRs(self):
+        df_links_bdrs = pd.DataFrame(self.info_pags_bdrs).set_index('nome')
+        df_links_bdrs.to_excel(self.end_arq_info_pags_bdrs)
+        gera_saida_em_excel(
+            {'Lista_Links_BDRs': df_links_bdrs},
+            self.end_arq_detalhes_bdrs
+        )
+        return df_links_bdrs
+    
+    def salva_excel_detalhes_BDRs(self):
+        df_detalhes_bdrs = pd.DataFrame(self.lista_detalhes_bdrs).set_index('nome')
+        gera_saida_em_excel(
+            {'Detalhes_BDRs': df_detalhes_bdrs},
+            self.end_arq_detalhes_bdrs
+        )
+        return df_detalhes_bdrs
 
 
 ###################################################################
 #                            Execução                             #
 ###################################################################
-max_bdrs_a_coletar = 0 # 0 = máximo
+max_bdrs_a_coletar = 15 # 0 = máximo
 
-with AcessoBDRsB3() as edge:
-    edge.abre_pag_inicial_BDRs()
-    edge.gera_tabela_info_BDRs(max=max_bdrs_a_coletar)
-    edge.salva_excel_info_pags_BDRs()
+# Monta planilha de links
+# with AcessoBDRsB3() as edge:
+#     edge.abre_pag_inicial_BDRs()
+#     edge.gera_tabela_info_BDRs(max=max_bdrs_a_coletar)
+#     edge.salva_excel_info_pags_BDRs()
 
+# Monta planilha de detalhes
 with AcessoBDRsB3() as edge:
     edge.abre_pag_inicial_BDRs()
     edge.gera_detalhes_lista_BDRs(max=max_bdrs_a_coletar)
